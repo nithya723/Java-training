0883ecd27a80a41ce59572fabee1f3b23548df0b
nithya723 <73656068+nithya723@users.noreply.github.com>
2021-09-03 20:33:15 +0530

    Create testclass.java

---
+++ testclass.java
@@ -0,0 +1,14 @@
+class Test { }
+	
+
+	class Main1
+	{
+		public static void main(String[] args)
+		{
+			Test tobj = null;
+			if (tobj instanceof Test)
+			System.out.println("tobj is instance of Test");
+			else
+			System.out.println("tobj is NOT instance of Test");
+		}
+	}
