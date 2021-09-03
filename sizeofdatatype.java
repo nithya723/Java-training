75826550e86d4f2e373e96ca100f15891485b299
nithya723 <73656068+nithya723@users.noreply.github.com>
2021-09-03 20:25:53 +0530

    Create sizeofdatatype.java

---
+++ sizeofdatatype.java
@@ -0,0 +1,13 @@
+class size
+	{
+	        public static void main (String[] args)
+	        {
+	                System.out.println("size of integer:"+ (Integer.SIZE/8)+" bytes");
+	                System.out.println("size of byte   :"+ (Byte.SIZE/8)+" byte");
+	                System.out.println("size of float  :"+ (Float.SIZE/8)+" bytes");
+	                System.out.println("size of double :"+ (Double.SIZE/8)+" bytes");
+	                System.out.println("size of Short  :"+ (Short.SIZE/8)+" bytes");
+	                System.out.println("size of long   :"+ (Long.SIZE/8)+" bytes");
+	                System.out.println("size of char   :"+ (Character.SIZE/8)+" bytes");
+	        }
+	}
